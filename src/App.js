97e52fa33a2d606584517f21d534bdb2dd8daefe
Yashoda Bane <yeshoda.b@gmail.com>
2021-05-25 22:10:50 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+
+    const [data, setData] = useState([]);
+
+    const getCovidData = async () => {
+      try{
+        const res =await fetch('https://api.covid19india.org/data.json');
+        const actualData = await res.json();
+        console.log(actualData.statewise);
+        setData(actualData.statewise);
+      }
+      catch(err)
+      {
+        console.log(err);
+      }
+    }
+
+    useEffect(() =>{
+      getCovidData();
+    }, []);
+
+    return (
+      <>
+        
+        <div className="container-fluid mt-5">
+          <div className="main-heading">
+            <h1 className="mb-5 text-center"> <span className="font-weight-bold"> INDIA </span> COVID-19 DASHBOARD </h1>
+            <div className="table-responsive">
+              <table className="table table-hover">
+                <thead className="thead-dark">
+                  <tr>
+                    <th>State</th>
+                    <th>Confirmed</th>
+                    <th>Recovered</th>
+                    <th>Death</th>
+                    <th>Active</th>
+                    <th>Updated</th>
+                  </tr>
+                </thead>
+                <tbody>
+
+                  {data.map((curElem, ind) => {
+                    return (
+                      <tr key={ind}>
+                        <td>{curElem.state}</td>
+                        <td>{curElem.confirmed}</td>
+                        <td>{curElem.recovered}</td>
+                        <td>{curElem.deaths}</td>
+                        <td>{curElem.active}</td>
+                        <td>{curElem.lastupdatedtime}</td>
+                      </tr>
+                    )
+                  })
+                  }
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </> 
+    )
 }
 
-export default App;
+export default App
